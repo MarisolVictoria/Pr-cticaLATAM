ffd63981540fae21905f0e4860fd583f79a92325
Marisol Victoria Sáez Oportus <msaezopo@emeal.nttdata.com>
2021-12-29 19:32:14 +0100

    Se sube actividad de Mi Primer Programa

---
+++ MiPrimerPrograma/src/MiPrimerPrograma.java
@@ -0,0 +1,16 @@
+
+public class MiPrimerPrograma {
+
+    public static void main(String[] args) {
+    	String nombre = "Marisol";
+    	int edad = 20;
+
+        System.out.println("Mi nombre es Marisol");
+        System.out.println("Tengo 20 años de edad.");
+        System.out.println("Mi ciudad es Concepción, Chile");
+
+    }
+
+} 
+
+
