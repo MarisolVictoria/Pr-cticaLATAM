0f5c1028c242a5b738d64f5c7486eb31264e87dd
Marisol Victoria Sáez Oportus <msaezopo@emeal.nttdata.com>
2022-01-05 15:24:08 +0100

    Se realizaron los ejercicios de manipulación de cadenas

--- ManipulaciondeCadenas/src/Application.java
+++ ManipulaciondeCadenas/src/Application.java
@@ -1,18 +1,26 @@
 
 public class Application {
-	 public static void main(String[] args) {
-		 
-		 StringManipulator stringManipulator = new StringManipulator();
-		 String resultadollamadoMetodo = stringManipulator.trimAndConcat  (" Hola Mundo " , " Adiós Feliz año ");
-		 int obtenerindiceMetodo =stringManipulator.getIndexOrNull ("Te Amo", 'T');
+	public static void main(String[] args) {
 
-	
+		StringManipulator stringManipulator = new StringManipulator();
+		// String resultadollamadoMetodo = stringManipulator.trimAndConcat (" Hola Mundo
+		// " , " Adiós Feliz año ");
+		// int obtenerindiceMetodo =stringManipulator.getIndexOrNull ("Te Amo", 'T');
+		// int obtenerindiceMetodo1 = getIndexOrNull2 ("Buen día", 'e');
 
-	        
-	        
-	        System.out.println(resultadollamadoMetodo);
-	        System.out.println(obtenerindiceMetodo);
-	    }
+		// stringManipulator.getIndexOrNull("Welcome 2022", 'e');
 
-	} 
+		// stringManipulator.getIndexOrNull2("Que tal", "t");
 
+		/*
+		 * System.out.println(resultadollamadoMetodo);
+		 * System.out.println(obtenerindiceMetodo);
+		 * System.out.println(obtenerindiceMetodo1); {
+		 * 
+		 * }
+		 */
+		stringManipulator.concatSubstring("Buenos dias, hola como estas,", 9, 28, "espero que bien.");
+		stringManipulator.concatSubstring("Que tal", 4, 7, "Bienvenido");
+
+	}
+}
--- ManipulaciondeCadenas/src/StringManipulator.java
+++ ManipulaciondeCadenas/src/StringManipulator.java
@@ -1,33 +1,50 @@
-	 /*Elimina los espacios en blanco de ambos lados de las cadenas ingresadas y las concatena. 
-	  * Devolver la nueva cadena. Puede utilizar el método trim de la clase String.*/
+/*Elimina los espacios en blanco de ambos lados de las cadenas ingresadas y las concatena. 
+ * Devolver la nueva cadena. Puede utilizar el método trim de la clase String.*/
 
 public class StringManipulator {
-	
-      public String trimAndConcat (String Cadena1, String Cadena2) {
-    	  String resultado = null;
-    	  resultado = Cadena1.trim() + Cadena2.trim(); 
-    	  return resultado;
-      }
-      /*Obtener el índice del caracter dado y devolverlo o devolver -1.
-       *  Si el caracter aparece varias veces, retornar el primer índice. 
-       *  Puede utilizar el método indexOf de la clase String.*/
-       
-public class StringMnipulator  {
-	 public int obtenerindiceMetodo = getIndexOrNull ("Te Amo", 'A');
-     
-     public int getIndexOrNull (String frase, char letra) {
-       int resultado;
-    	 resultado = frase.indexOf(letra); 
-    	 return resultado; 
-     }
-}
-          
-      /*Obtener el índice donde empieza la subcadena dada y devolverlo o devolver -1. 
-       *Puede utilizar el método indexOf de la clase String.
-        StringManipulator manipulator = new StringManipulator();*/
 
-public class StringManipulator  {
-     
-	
-}
+	public String trimAndConcat(String Cadena1, String Cadena2) {
+		String resultado = null;
+		resultado = Cadena1.trim() + Cadena2.trim();
+		return resultado;
+	}
+	/*
+	 * Obtener el índice del caracter dado y devolverlo o devolver -1. Si el
+	 * caracter aparece varias veces, retornar el primer índice. Puede utilizar el
+	 * método indexOf de la clase String.
+	 */
 
+	public int getIndexOrNull(String frase, char letra) {
+		int resultado;
+		resultado = frase.indexOf(letra);
+		System.out.println(resultado);
+		return resultado;
+
+	}
+	/*
+	 * Obtener el índice donde empieza la subcadena dada y devolverlo o devolver -1.
+	 * Puede utilizar el método indexOf de la clase String.
+	 */
+
+	public void getIndexOrNull2(String subCadena1, String subCadena2) {
+		int obtenerindiceMetodo = subCadena1.indexOf(subCadena2);
+		System.out.println(obtenerindiceMetodo);
+
+	}
+
+	/*
+	 * Obtener una subcadena utilizando un índice de inicio y un índice de
+	 * finalización y concatenarlo con la segunda cadena de entrada en nuestro
+	 * método. Puede utilizar el método substring de la clase String.
+	 */
+
+	public String concatSubstring(String textoInicial, int beginIndex, int endIndex, String textoFinal) {
+		String resultado = null;
+
+		resultado = textoInicial.substring(beginIndex, endIndex).concat(textoFinal);
+
+		System.out.println(resultado);
+		return resultado;
+	}
+
+}
